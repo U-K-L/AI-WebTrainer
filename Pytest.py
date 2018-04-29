9568f48f9e74833f988eed6552c694b5ea4cdecf
UKL <WilliamsNoah1720@gmail.com>
2018-04-29 18:43:17 -0400

    Restructered all files to be compatible with flask!

--- Pytest.py
+++ Pytest.py
@@ -1,9 +1,10 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+
 
 
 engine = create_engine("postgres://kwyoldtqbouptt:f7127c443551c5b8d363c91d3fd54dd7c9967d64db7e40d82b9d6ea371366b23@ec2-174-129-28-38.compute-1.amazonaws.com:5432/d6g2qqn2orkcf2")
@@ -14,8 +15,8 @@ db = scoped_session(sessionmaker(bind = engine))
 #Method tied to app route.
 
 def index():
-    names = db.excute("CREATE TABLE flight")
-    return "Hello World"
+    #names = db.excute("CREATE TABLE flight")
+    return render_template('index.html')
     
 if __name__ == "__main__":
     app.run(debug=True)
